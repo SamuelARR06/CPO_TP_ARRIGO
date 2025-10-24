87da57c5f4ad1be517f53a96f31752c87f8b9fa1
Samuel Arrigo <samarrigo06@gmail.com>
2025-10-24 11:13:59 +0200

    TP0 Hello world

---
+++ TP0_ helloWorld/src/tp0_/helloworld/TP0_HelloWorld.java
@@ -0,0 +1,29 @@
+/*helloword, afficher un message, 20/10/25*/
+package tp0_.helloworld;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author samar
+ */
+public class TP0_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+         
+        
+       String prenom;
+Scanner sc ;
+sc = new Scanner (System.in) ;
+System.out.println("Quel est votre prénom ?");
+prenom = sc.nextLine(); 
+System.out.println("aurevoir");
+        // TODO code application logic here
+    }
+
+    
+}
