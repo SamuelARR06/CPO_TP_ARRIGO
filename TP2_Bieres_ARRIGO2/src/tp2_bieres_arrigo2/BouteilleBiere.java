a93e57b90cda93ecba22c420757a0959e2de8824
Samuel Arrigo <samarrigo06@gmail.com>
2025-12-15 15:46:24 +0100

    TP2 bierre

---
+++ TP2_Bieres_ARRIGO2/src/tp2_bieres_arrigo2/BouteilleBiere.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_arrigo2;
+
+/**
+ *
+ * @author samar
+ */
+public class BouteilleBiere {
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
+
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+        nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false;
+        
+    }
+
+    public void lireEtiquette() {
+        System.out.println("Bouteille de " + nom + " (" + degreAlcool + " degres) \nBrasserie : " + brasserie);
+    }
+
+    public boolean decapsuler() {
+        if (!ouverte) {
+            ouverte = true;
+            return true;
+        } else {
+            System.out.println("Erreur : biere déjà ouverte");
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String chaine_a_retourner;
+        chaine_a_retourner = nom + " (" + degreAlcool + " degres) Ouverte ? ";
+        if (ouverte) chaine_a_retourner += "oui";
+        else chaine_a_retourner += "non";
+        return chaine_a_retourner;
+    }
+}
+
+
